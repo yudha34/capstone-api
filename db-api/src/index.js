e1c145c04dd2375994d48a47ca86073b57520838
yudha34 <anggoroyudap34@gmail.com>
2023-06-08 00:20:43 +0700

    add get all

--- db-api/src/index.js
+++ db-api/src/index.js
@@ -1,2 +1,29 @@
-const data = "yaudin";
-console.log(data);
+const express = require('express')
+const app = express()
+const port = 3001
+const bodyParser = require('body-parser')
+const connection = require('./connection')
+const response = require('./response')
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  const sql = "SELECT * FROM equipment"
+  connection.query(sql, (err, result) => {
+    //hasil dari query mysql
+    response(200, result, "get all data from equipment", res)
+  })
+
+})
+
+app.post('/search', (req, res) => {
+  console.log({requestOutside: req.body})
+  const name = req.body.name
+  if (req.body.name === "yudha")
+  res.send(`Login berhasil ${name}`)
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
